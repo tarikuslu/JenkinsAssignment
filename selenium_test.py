e0c4114d825a6cfffc23e3de443350c8b828fec1
tarikuslu <58303314+tarikuslu@users.noreply.github.com>
2025-03-13 23:11:55 +0300

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -26,30 +26,30 @@ try:
 
     # 2. "Kategoriler" menüsüne tıkla
     kategoriler_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Kategoriler')]"))
+        EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'KOZMETİK')]"))
     )
     kategoriler_button.click()
-    print("Kategoriler menüsüne tıklandı!")
+    print("Kozmetik Kategorileri menüsüne tıklandı!")
 
     # 3. "Erkek" kategorisini seç
-    erkek_kategori = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Erkek')]"))
+    parfüm_kategori = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'PARFÜM VE DEODORANT')]"))
     )
-    erkek_kategori.click()
-    print("Erkek kategorisi seçildi!")
+    parfüm_kategori.click()
+    print("Parfüm ve deodorant kategorisi seçildi!")
 
     # 4. Arama çubuğuna "gömlek" yaz ve ara
     arama_cubugu = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "search"))
+        EC.presence_of_element_located((By.ID, "search-form__input-field__search-input"))
     )
     arama_cubugu.send_keys("gömlek")
-    arama_button = driver.find_element(By.XPATH, "//button[@class='search-button']")
+    arama_button = driver.find_element(By.CSS_SELECTOR, "div .search-form__input-field__btn-search")
     arama_button.click()
     print("Arama yapıldı: gömlek")
 
     # 5. Arama sonuçlarının yüklendiğini doğrula (assertion)
     sonuc_sayisi = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "product-list-info"))
+        EC.presence_of_all_elements_located((By.CLASS_NAME, "product-list-heading__product-count__count"))
     )
     assert sonuc_sayisi.is_displayed(), "Arama sonuçları yüklenmedi!"
     print("Arama sonuçları başarıyla yüklendi!")
